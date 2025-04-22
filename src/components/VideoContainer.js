199e980a4805abc9982ccf2406bd03bfd0d115a6
chetanh2 <chetan@osiansoftware.com>
2025-04-22 11:21:47 +0530

    fixed route menu open tggle

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { YOUTUBE_VIDEOS_API } from '../utils/constant'
-import VideoCard, { AdVideoCard } from './VideoCard'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constant";
+import VideoCard, { AdVideoCard } from "./VideoCard";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { closeMenu, openMenu, toggleMenu } from "../utils/appSlice";
 
 const VideoContainer = () => {
-  const [videos,setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const isMenuOpen = useSelector((state) => state.app.isMenuOpen);
 
-  useEffect(()=>{
-    getVideos()
-  },[])
+  useEffect(() => {
+    getVideos();
+  }, []);
 
-  const getVideos = async ()=>{
-    const data = await fetch(YOUTUBE_VIDEOS_API)
-    const json = await data.json()
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
     console.log(json.items);
-    setVideos(json.items)
-  }
+    setVideos(json.items);
+  };
+  useEffect(() => {
+    dispatch(openMenu());
+  }, [isMenuOpen, location]);
+
   return (
     <div className="flex flex-wrap mt-2 ">
       {/* <VideoCard info={videos[0]}/> */}
-      {videos[0] &&<AdVideoCard info={videos[0]}/>}
+      {videos[0] && <AdVideoCard info={videos[5]} />}
       {videos.map((video) => (
         <Link className="w-[24%]" to={"/watch?v=" + video.id}>
           <VideoCard key={video.id} info={video} />
@@ -27,6 +36,6 @@ const VideoContainer = () => {
       ))}
     </div>
   );
-}
+};
 
-export default VideoContainer
+export default VideoContainer;
